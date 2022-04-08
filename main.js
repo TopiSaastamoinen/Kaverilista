605243095a9763403425aa3055eb6cfc3fe9a2bb
Topi Saastamoinen <topi.saastamoinen@esedulainen.fi>
2022-04-08 09:55:09 +0300

    Poistaa nyt hakukenttään kirjoitetun nimen

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let kaverit = [];
 
-
 function addFriend() {
     console.log('add friend')
     if (document.querySelector('#main input[type="search"]').value <= 0) {
@@ -29,19 +28,22 @@ function addFriend() {
 }
 
 function deleteFriend() {
-    if (kaverit.length <= 0) {
-        alert("Sinulla ei ole kavereita")
-
-        // Estää sivua latautumasta uudelleen
-        event.preventDefault()
-    } else {
-        // Estää sivua latautumasta uudelleen
-        event.preventDefault()
-        console.log('delete friend')
-
-        kaverit.pop();
-        document.querySelector('#friendList').lastElementChild.remove();
+    // Estää sivua latautumasta uudelleen
+    event.preventDefault()
+    console.log('delete friend')
+    // Tallentaa hakukenttään kirjoitetun nimen
+    let kaverinNimi = document.querySelector('#main input[type="search"]').value;
+    let i = 0;
+    while (kaverit.length > i) {
+        console.log('Finding it')
+        if (kaverinNimi == kaverit[i]) {
+            kaverit.splice(i, 1)
+            document.querySelector('#friendList').lastElementChild.remove();
+        }
+        i++;
     }
+
+    printFriends();
 }
 
 function arrangeFriends() {
@@ -58,26 +60,28 @@ function arrangeFriends() {
         // Järjestää kaverit-muuttujan nimet aakkosjärjestykseen
         kaverit.sort()
 
-        // Tyhjentää käyttäjälle näkyvän listan
-        let i = 0;
-        while (kaverit.length > i) {
-            document.querySelector('#friendList').lastElementChild.remove();
-            console.log('making space');
-            i++;
-        }
-
-        // Tulostaa järjestetyn kaverit-muuttujan nimet
-        i = 0;
-        while (kaverit.length > i) {
-            let uusiElementti = document.createElement('li')
-            let uusiElementtiTeksti = document.createTextNode(kaverit[i])
-            uusiElementti.appendChild(uusiElementtiTeksti)
-
-            document.querySelector('#friendList').appendChild(uusiElementti)
-            console.log('printing sorted list');
-            i++;
-        }
+        printFriends();
     }
 }
 
+function printFriends() {
+    // Tyhjentää käyttäjälle näkyvän listan
+    let i = 0;
+    while (kaverit.length > i) {
+        document.querySelector('#friendList').lastElementChild.remove();
+        console.log('making space');
+        i++;
+    }
 
+    // Tulostaa järjestetyn kaverit-muuttujan nimet
+    i = 0;
+    while (kaverit.length > i) {
+        let uusiElementti = document.createElement('li')
+        let uusiElementtiTeksti = document.createTextNode(kaverit[i])
+        uusiElementti.appendChild(uusiElementtiTeksti)
+
+        document.querySelector('#friendList').appendChild(uusiElementti)
+        console.log('printing sorted list');
+        i++;
+    }
+}
